bc45c0cb167e3a986fdab18f6edeaf2224aefbf2
Rafael-Diaz-O <rafadz102@gmail.com>
2025-08-30 14:56:07 -0500

    Delete Videos directory

--- Videos/Especies/src/especies/Mamifero.java
+++
@@ -1,9 +0,0 @@
-package especies;
-
-
-public abstract class Mamifero extends AnimalesVertebrados {
-    public Mamifero (String color,int numeroDePatas){
-        super(color,numeroDePatas);
-        
-    }
-}
